c234423863ba739659c0e66ea2daf456e01f567a
abrabant <abrabant@student.42.fr>
2021-11-04 08:21:39 +0100

    Add BSQ map generator script

---
+++ algorithms/BSQ/genmap.sh
@@ -0,0 +1,21 @@
+#! /bin/sh
+
+# Generate a BSQ map given its x and y dimensions.
+# Each cell has either the value 0 or 1.
+
+if [ $# -ne 2 ]; then
+	printf "USAGE: ./genmap <x> <y>\n";
+	exit 1
+fi
+
+x=$1; y=$2
+
+for i in $(seq 1 $y);
+do
+	for i in $(seq 1 $x);
+	do
+		r=$((RANDOM % 2))
+		[ $r -eq 0 ] && printf '0' || printf '1'
+	done
+	printf '\n';
+done
